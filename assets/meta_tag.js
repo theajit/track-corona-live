f5cfca224059d3ead10edec80d926cc9ae9b0ca5
Ajit Satpathy <ajit@xola.com>
2020-04-17 03:14:06 +0530

    format css and html

--- assets/meta_tag.js
+++ assets/meta_tag.js
@@ -1,14 +1,14 @@
-var metaTag=document.createElement('meta');
+var metaTag = document.createElement('meta');
 metaTag.name = "viewport"
 metaTag.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0"
 document.getElementsByTagName('head')[0].appendChild(metaTag);
 
-var metaTagDescription=document.createElement('meta');
+var metaTagDescription = document.createElement('meta');
 metaTagDescription.name = "description"
 metaTagDescription.content = "Track corona in India and Indian states with a live online and interactive dashboard, visualize with map and table of India, World and the USA.  It also consists of India's helpline number and Statewise case data."
 document.getElementsByTagName('head')[0].appendChild(metaTagDescription);
 
-var metaTagKeywords=document.createElement('meta');
+var metaTagKeywords = document.createElement('meta');
 metaTagKeywords.name = "keywords"
 metaTagKeywords.content = "track corona india, track corona maharastra, track corona delhi, track corona mumbai, covid19india, track corona online, track corona live,corona map,coronavirus, covid-19, corona tracker,corona dashboard, corona preperation, corona prevention, corona trends, corona statistics, corona data, China, USA, World, India"
 document.getElementsByTagName('head')[0].appendChild(metaTagKeywords);
